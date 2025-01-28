9dcb461b492867fe3d729ee811526133b117e64c
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2025-01-28 17:51:03 +0700

    untuk melakukan operasi CRUD (Create, Read, Update, Delete) di data kandang.

---
+++ app/src/main/java/com/example/zoohealth/repository/KandangRepository.kt
@@ -0,0 +1,54 @@
+package com.example.zoohealth.repository
+
+import com.example.zoohealth.model.AllKandangResponse
+import com.example.zoohealth.model.Kandang
+import com.example.zoohealth.model.KandangDetailResponse
+import com.example.zoohealth.service_api.KandangService
+import java.io.IOException
+
+interface KandangRepository {
+    suspend fun insertKandang(kandang: Kandang)
+
+    suspend fun getKandang(): AllKandangResponse
+
+    suspend fun updateKandang(idKandang: String, kandang: Kandang)
+
+    suspend fun deleteKandang(idKandang: String)
+
+    suspend fun getKandangById(idKandang: String): KandangDetailResponse
+}
+
+class NetworkKandangRepository(
+    private val kandangApiService: KandangService
+) : KandangRepository {
+    override suspend fun insertKandang(kandang: Kandang) {
+        kandangApiService.insertKandang(kandang)
+    }
+
+    override suspend fun updateKandang(idKandang: String, kandang: Kandang) {
+        kandangApiService.updateKandang(idKandang, kandang)
+    }
+
+    override suspend fun deleteKandang(idKandang: String) {
+        try {
+            val response = kandangApiService.deleteKandang(idKandang)
+            if (!response.isSuccessful) {
+                throw IOException(
+                    "Failed to delete Kandang. HTTP Status code: ${response.code()}"
+                )
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getKandang(): AllKandangResponse =
+        kandangApiService.getAllKandang()
+
+    override suspend fun getKandangById(idKandang: String): KandangDetailResponse {
+        return kandangApiService.getKandangById(idKandang)
+    }
+}
